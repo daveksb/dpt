5ce9806fba6a3ebdda256ef462170b9a401bfe9b
Dave <akkradet@profilegroup-hrs.com>
2022-07-29 14:36:45 +0700

    create first lib

--- apps/dpt-ui/src/app/app.module.ts
+++ apps/dpt-ui/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LoginComponent } from '@dpt/feature/login';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, LoginComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
