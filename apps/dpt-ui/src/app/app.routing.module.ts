ebb4a0a92f421aef719a09535ea72d652b51a699
Dave <akkradet@profilegroup-hrs.com>
2022-08-06 14:39:40 +0700

    pdpa

--- apps/dpt-ui/src/app/app.routing.module.ts
+++ apps/dpt-ui/src/app/app.routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '@dpt/feature/login';
+import { FormRegisterComponent } from '@dpt/form';
 import { PdpaComponent } from '@dpt/ui';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'pdpa', component: PdpaComponent },
+  { path: 'register', component: FormRegisterComponent },
 
   { path: '**', component: LoginComponent },
 ];
--- libs/ui/src/pdpa/pdpa.component.ts
+++ libs/ui/src/pdpa/pdpa.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'dpt-pdpa',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './pdpa.component.html',
   styleUrls: ['./pdpa.component.scss'],
 })
