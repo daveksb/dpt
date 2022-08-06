362c51cf07bafdaf1da0010f1f3275a52116efb6
Dave <akkradet@profilegroup-hrs.com>
2022-08-06 15:46:34 +0700

    start form register

--- libs/form/src/form-register/form-register.component.ts
+++ libs/form/src/form-register/form-register.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'dpt-form-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './form-register.component.html',
   styleUrls: ['./form-register.component.scss'],
 })
