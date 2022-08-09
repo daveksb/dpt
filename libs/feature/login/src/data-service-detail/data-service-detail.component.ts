31c855fae0f313c4b327dc74375a48b3e78ef2fb
Dave <akkradet@profilegroup-hrs.com>
2022-08-09 22:48:51 +0700

    add material

--- libs/feature/login/src/data-service-detail/data-service-detail.component.ts
+++ libs/feature/login/src/data-service-detail/data-service-detail.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataServiceDialogComponent, TopNavComponent } from '@dpt/ui';
 import { RouterModule } from '@angular/router';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'dpt-data-service-detail',
   standalone: true,
-  imports: [CommonModule, TopNavComponent, RouterModule],
+  imports: [CommonModule, TopNavComponent, RouterModule, MatDialogModule],
   templateUrl: './data-service-detail.component.html',
   styleUrls: ['./data-service-detail.component.scss'],
 })
 export class DataServiceDetailComponent {
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
- /*  openDialog() {
+  openDialog() {
     this.dialog.open(DataServiceDialogComponent, {
       height: '400px',
       width: '600px',
     });
-  } */
+  }
 }
